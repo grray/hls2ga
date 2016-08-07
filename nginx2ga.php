ac6af3d0d845fd6d8611d4cb713c96c695643d28
Smirnov Sergey <ggrraay@gmail.com>
2016-08-07 11:25:35 +0100

    Added queue delay

--- nginx2ga.php
+++ nginx2ga.php
@@ -1,12 +1,13 @@
 <?php
 
-require(__DIR__ . "/config.sample.php");
+require(__DIR__ . "/config.php");
 
 $reporter = new Reporter();
 $parser = new Parser($argv[1]);
 while($hit = $parser->fetch()) {
   $reporter->reportHit($hit);
 }
+
 
 class Parser {
   private $fp;
@@ -35,7 +36,6 @@ class Parser {
 
       $ip = $rec[$conf['ip_pos']];
       if ($ip == "127.0.0.1") continue;  // skip local stuff
-
       $request = explode(" ", $rec[$conf['request_pos']]);
       if (!isset($request[1])) {
         fwrite(STDERR, "Wrong request: {$rec[$conf['request_pos']]}\n");
@@ -46,17 +46,21 @@ class Parser {
       $pathInfo = pathinfo($url);
     } while(!isset($pathInfo['extension']) || $pathInfo['extension'] != 'ts');
 
-
+    $ua = $rec[$conf['ua_pos']];
+    $cid = $rec[$conf['cookie_pos']];
+    if ($cid == "" || $cid == "-") {
+      $cid = md5($ip.$ua);  // no cookie, generate some id from IP and UA
+    }
 
     return array(
       'uip' => $ip,
-      'ua' => $rec[$conf['ua_pos']],
+      'ua' => $ua,
       // event label, use first part filename ("medium" for medium-1635.ts)
       'el' => explode("-", $pathInfo['basename'])[0],
       // event value, use filesize
       'ev' => $rec[$conf['size_pos']],
       'dr' => $rec[$conf['referer_pos']],
-      'cid' => $rec[$conf['cookie_pos']],
+      'cid' => $cid,
     );
   }
 }
@@ -64,6 +68,7 @@ class Parser {
 class Reporter {
   private $payload = "";
   private $payloadHits = 0;
+  private $payloadTime = array();
 
   public function reportHit($hit) {
     global $conf;
@@ -85,11 +90,12 @@ class Reporter {
    */
   private function reportPayload($payload) {
     // check if 16Kb payload limit exceeded
-    if (strlen($this->payload)+strlen($payload) > 16*1024) {
+    if (strlen($this->payload)+strlen($payload) > (16*1024-10*20)) { // 10*20 is reserved for &qt=100000
       $this->flushPayload();
     }
     $this->payload .= $payload."\n";
     $this->payloadHits++;
+    $this->payloadTime[] = microtime(true);
     // check if 20 hits limit exceeded
     if ($this->payloadHits>=20) {
       $this->flushPayload();
@@ -100,13 +106,21 @@ class Reporter {
    * report batch payload
    */
   private function flushPayload() {
+    // add &qt (queue times) to payload
+    $time = microtime(true);
+    $payload = explode("\n", $this->payload);
+    foreach ($payload as $key=>$value) {
+      $payload[$key] .= "&qt=".ceil(($time - $this->payloadTime[$key]) * 1000);
+    }
+    $payload = join("\n", $payload);
+
     $url = 'http://www.google-analytics.com/batch';
 
     $context = stream_context_create(array(
       'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'  => 'POST',
-        'content' => $this->payload
+        'content' => $payload
       )
     ));
     $result = file_get_contents($url, false, $context);
@@ -117,6 +131,7 @@ class Reporter {
 
     $this->payload = "";
     $this->payloadHits = 0;
+    $this->payloadTime = array();
   }
 
 }
